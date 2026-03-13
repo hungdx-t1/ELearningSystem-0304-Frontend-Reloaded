e1a540ba24a4849595231dc365edc1feb54994f7
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-03-13 11:10:08 +0700

    update routing outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Nếu vào trang chủ (localhost:4200) thì tự động chuyển hướng sang trang login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  { path: 'login', component: Login },
+  
+  // TODO: (Sau này các trang khác như Dashboard, Course sẽ thêm vào dưới này)
+];

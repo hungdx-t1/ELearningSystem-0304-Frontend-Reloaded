cefa64222d9bdda1afe55db961e4a492a4fde82b
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-03-13 10:15:31 +0700

    added JWT interceptor

---
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  // Lấy token từ bộ nhớ trình duyệt
+  const token = localStorage.getItem('token');
+
+  // Nếu có token, kẹp nó vào Header của request gửi đi
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(cloned);
+  }
+
+  // Nếu không có, cứ gửi request đi bình thường (ví dụ: lúc mới đăng nhập)
+  return next(req);
+};

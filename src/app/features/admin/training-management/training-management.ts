0bdaa3d0588fb366aa53c125bf23e1208252bcf5
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-03-18 15:21:53 +0700

    create admin components

---
+++ src/app/features/admin/training-management/training-management.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training-management',
+  imports: [],
+  templateUrl: './training-management.html',
+  styleUrl: './training-management.scss',
+})
+export class TrainingManagement {
+
+}

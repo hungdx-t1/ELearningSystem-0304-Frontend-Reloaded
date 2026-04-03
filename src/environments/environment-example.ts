ed831849f7a01cecc18042283d1f404f02e76459
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-04-03 10:50:19 +0700

    added firebase package

---
+++ src/environments/environment-example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSy...",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcd..."
+  }
+};

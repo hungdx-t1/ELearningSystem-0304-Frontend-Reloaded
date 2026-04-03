92639a0f741dd8c327d983cb48de9c3dc7d02194
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-04-03 15:21:37 +0700

    temporary fix realtime chat service

--- src/app/core/services/realtime-chat.service.ts
+++ src/app/core/services/realtime-chat.service.ts
@@ -1,5 +1,5 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Injectable, PLATFORM_ID, inject } from '@angular/core';
+import { Injectable, PLATFORM_ID, Injector, inject, runInInjectionContext } from '@angular/core';
 import { Firestore, collection, addDoc, collectionData, query, orderBy, serverTimestamp } from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
 
@@ -17,9 +17,9 @@ export interface ChatMessage {
 })
 export class RealtimeChatService {
   private firestore = inject(Firestore);
-
-  // Tiêm "Máy dò môi trường" vào đây
   private platformId = inject(PLATFORM_ID);
+
+  private injector = inject(Injector); // temp 
 
   /**
    * Lắng nghe tin nhắn của một Lớp học (Real-time)
@@ -31,14 +31,17 @@ export class RealtimeChatService {
       return of([]); // Trả về mảng rỗng để Node.js khỏi bị "ngáo"
     }
 
-    // Trỏ tới đúng folder tin nhắn của lớp đó trên Firebase
-    const messagesRef = collection(this.firestore, `class_chats/${classId}/messages`);
-    
-    // Sắp xếp tin nhắn theo TG (cũ nhất ở trên, mới nhất ở dưới)
-    const q = query(messagesRef, orderBy('timestamp', 'asc'));
-    
-    // Trả về luồng dữ liệu (Kèm theo cái ID của document)
-    return collectionData(q, { idField: 'id' }) as Observable<ChatMessage[]>;
+    // Bọc hàm của Firebase vào trong Injection Context để lách luật Angular
+    return runInInjectionContext(this.injector, () => {
+      // Trỏ tới đúng folder tin nhắn của lớp đó trên Firebase
+      const messagesRef = collection(this.firestore, `class_chats/${classId}/messages`);
+      
+      // Sắp xếp tin nhắn theo TG (cũ nhất ở trên, mới nhất ở dưới)
+      const q = query(messagesRef, orderBy('timestamp', 'asc'));
+      
+      // Trả về luồng dữ liệu (Kèm theo cái ID của document)
+      return collectionData(q, { idField: 'id' }) as Observable<ChatMessage[]>;
+    });
   }
 
   /**

5b2571541acb8f447755f8ddf6a508e042cc0d12
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-04-03 13:59:21 +0700

    update student course detail component (for firebase hooking)

--- src/app/features/student/courses/course-detail-component/course-detail-component.ts
+++ src/app/features/student/courses/course-detail-component/course-detail-component.ts
@@ -1,12 +1,16 @@
 // CourseDetailComponent on Student side
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CourseService, Course, Chapter } from '../../../../core/services/course.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { ChatMessage, RealtimeChatService } from '../../../../core/services/realtime-chat.service';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-course-detail-component',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, DatePipe],
   templateUrl: './course-detail-component.html',
   styleUrl: './course-detail-component.scss',
   animations: [
@@ -17,18 +21,40 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
     ])
   ]
 })
-export class CourseDetail implements OnInit {
+export class CourseDetail implements OnInit, AfterViewChecked {
   private route = inject(ActivatedRoute);
   private courseService = inject(CourseService);
+
+  public authService = inject(AuthService);
+  private chatService = inject(RealtimeChatService);
 
   // Quản lý trạng thái bằng Signal
   course = signal<Course | null>(null);
   chapters = signal<Chapter[]>([]);
   isLoading = signal<boolean>(true);
+  
+  // chat variables
+  @ViewChild('chatScroll') private chatScrollContainer!: ElementRef;
+  classId = signal<string | null>(null);
+  messages = signal<ChatMessage[]>([]);
+  newMessage = signal<string>('');
+
+  currentUserId = this.authService.getCurrentUserId();
+  currentUserRole = this.authService.getUserRole();
+  currentUserName = this.authService.userProfile()?.fullName || 'Sinh viên';
 
   async ngOnInit() {
-    // 1. Lấy cái ID từ trên thanh địa chỉ (ví dụ: /courses/123 -> lấy số 123)
+    // Lấy cái ID từ trên thanh địa chỉ (ví dụ: /courses/123 -> lấy số 123)
     const courseId = this.route.snapshot.paramMap.get('id');
+
+    // Lấy classId từ trên URL (Ví dụ: /courses/123?classId=456)
+    this.route.queryParamMap.subscribe(params => {
+      const cId = params.get('classId');
+      if (cId) {
+        this.classId.set(cId);
+        this.loadChat(); // Có ID lớp thì bắt đầu lắng nghe tin nhắn
+      }
+    });
     
     if (courseId) {
       try {
@@ -53,6 +79,42 @@ export class CourseDetail implements OnInit {
       }
     }
   }
+
+  // chat functions
+  loadChat() {
+    const cId = this.classId();
+    if (!cId) return;
+    this.chatService.getClassMessages(cId).subscribe(msgs => {
+      this.messages.set(msgs);
+    });
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.chatScrollContainer.nativeElement.scrollTop = this.chatScrollContainer.nativeElement.scrollHeight;
+    } catch(err) { }
+  }
+
+  sendMessage() {
+    const cId = this.classId();
+    if (!this.newMessage().trim() || !cId) return;
+    
+    this.chatService.sendMessage(
+      cId,
+      this.currentUserId,
+      this.currentUserName,
+      this.currentUserRole,
+      this.newMessage().trim()
+    );
+    
+    this.newMessage.set('');
+  }
+
+  // course and class management functions
 
   // Hàm bật/tắt đóng mở Chương (Accordion)
   toggleChapter(chapter: Chapter) {

2e00cd18daa591ce6757f95f9c9e31ece40a48ee
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-04-08 10:07:32 +0700

    added environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://ten-backend-cua-ban.onrender.com/api', // link C# backend đã deploy trên Render.com
+  firebase: {
+    // TODO: Replace with my Firebase project configuration
+  }
+};
